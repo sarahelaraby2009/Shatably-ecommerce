f3bd5ad9a352645f4f512532239f5d906b6e9bf3
norangalil <norangalil@gmail.com>
2025-11-17 17:15:24 +0200

    update tailwind config after merging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-<<<<<<< HEAD
-        "./components/**/*.{vue,js,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
-  theme: {
-    extend: {
-          spacing: {
-        '90px': '90px',
-=======
     './components/**/*.{vue,js,ts}',
     './layouts/**/*.{vue,js,ts}',
     './pages/**/*.{vue,js,ts}',
@@ -23,7 +10,6 @@ export default {
     extend: {
       fontFamily: {
         poppins: ['"Poppins"', 'sans-serif'],
->>>>>>> main
       },
     },
   },
