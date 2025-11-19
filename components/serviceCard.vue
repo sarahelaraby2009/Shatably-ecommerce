40bd7df801d630af4772ad9f98f333f943f14e7b
norangalil <norangalil@gmail.com>
2025-11-19 20:00:01 +0200

    engineers dynamic pages done and service-review components done

---
+++ components/serviceCard.vue
@@ -0,0 +1,38 @@
+
+<template>
+
+<div class="w-[360px] bg-white rounded-2xl shadow-md overflow-hidden">
+  
+  <div class="w-full">
+    <img 
+      :src="service.image" 
+      class="w-full h-[200px] object-cover"
+    />
+    
+  </div>
+
+  <div class="p-4">
+    <h3 class="text-xl font-semibold">
+{{service.title}} 
+   </h3>
+    
+    <p class="text-gray-600 mt-2 leading-relaxed">
+     {{service.description}}
+
+    </p>
+  </div>
+
+</div>
+
+
+</template>
+
+<script setup>
+defineProps({
+service:{
+  type : Object,
+  required: true
+}
+
+});
+</script>
--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-
+import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
 library.add(fas, far, fab)
